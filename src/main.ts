6829e7a00a6d25b376972e9d2f2c3ae70b45f58a
johannes-z <johannes.zwirchmayr@gmail.com>
2022-06-23 12:57:58 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
+import Vue from 'vue'
+import { TestComponent } from './TestComponent'
 
-import { sp } from '@pnp/sp'
-
-function getPageContext () {
-  return window
-    // @ts-ignore
-    ?.spModuleLoader
-    ?._bundledComponents
-    ?.['b6917cb1-93a0-4b97-a84d-7cf49975d4ec']
-    ?.PageManager
-    ?._instance
-    ?.pageContext
-}
-
-sp.setup({
-  ie11: false,
-  sp: {
-    headers: {
-      Accept: 'application/json;odata=minimalmetadata',
-      'OData-Version': '3.0',
-    },
-    baseUrl: getPageContext()?.web?.absoluteUrl ?? window.location.origin,
-  },
-})
+new Vue({
+  render: h => h(TestComponent)
+}).$mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,30 @@
 import * as path from 'path'
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue2'
 
 export default defineConfig({
   optimizeDeps: {
     entries: 'src/dev.ts',
   },
   plugins: [
-    createVuePlugin({
+    vue({
       jsx: true,
-      vueTemplateOptions: {
+      template: {
         compilerOptions: {
           whitespace: 'condense',
         },
-      },
+      }
     }),
   ],
+  server: {
+    port: 8080,
+    host: 'localhost',
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: '8081',
+    },
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
